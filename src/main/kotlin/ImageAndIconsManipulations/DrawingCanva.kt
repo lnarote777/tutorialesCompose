78f93745260dd328fe3d444b35ebf8b1c126794c
lnarote777 <lnarote777@iesrafaelalberti.es>
2024-04-26 10:45:16 +0200

    tutorial imagen (se me olvido añadirlo)

---
+++ src/main/kotlin/ImageAndIconsManipulations/DrawingCanva.kt
@@ -0,0 +1,54 @@
+package ImageAndIconsManipulations
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Paint
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.graphics.withSave
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.res.loadImageBitmap
+import androidx.compose.ui.res.loadSvgPainter
+import androidx.compose.ui.res.loadXmlImageVector
+import androidx.compose.ui.res.useResource
+import org.xml.sax.InputSource
+
+@Composable
+fun TutorialImages03() {
+    val density = LocalDensity.current // to calculate the intrinsic size of vector images (SVG, XML)
+
+    val sample = remember {
+        useResource("sample.png", ::loadImageBitmap)
+    }
+    val ideaLogo = remember {
+        useResource("idea-logo.svg") { loadSvgPainter(it, density) }
+    }
+    val composeLogo = rememberVectorPainter(
+        remember {
+            useResource("compose-logo.xml") { loadXmlImageVector(InputSource(it), density) }
+        }
+    )
+
+    Canvas(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        drawIntoCanvas { canvas ->
+            canvas.withSave {
+                canvas.drawImage(sample, Offset.Zero, Paint())
+                canvas.translate(sample.width.toFloat(), 0f)
+                with(ideaLogo) {
+                    draw(ideaLogo.intrinsicSize)
+                }
+                canvas.translate(ideaLogo.intrinsicSize.width, 0f)
+                with(composeLogo) {
+                    draw(Size(100f, 100f))
+                }
+            }
+        }
+    }
+}
